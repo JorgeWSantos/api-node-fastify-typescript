2f8c166962065dacbba23979e9661aa2f6dd929e
JorgeWS <jorgews.dev@gmail.com>
2024-02-22 20:37:43 -0300

    build: 🏠 Configure knex

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+import { knex as setUpKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: './db/app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+const knex = setUpKnex(config)
+
+export default knex
